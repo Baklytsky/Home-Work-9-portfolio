ca3fcca51c8247d5926ee3919b9b3c77e8539708
Baklytsky <baklytsky@gmail.com>
2019-12-01 17:45:14 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleancss     = require('gulp-clean-css');
+const gulp       	= require('gulp');
+const sass 			= require('gulp-sass');
+const watch 		= require('gulp-watch');
+const autoprefixer 	= require('gulp-autoprefixer');
+const cleancss 		= require('gulp-clean-css');
+const browserSync 	= require('browser-sync').create();
 
+ 		
 
 gulp.task('styles', function () {
-    return gulp.src('./style/scss/**/*.scss')
+    return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 3 versions'], cascade: false}))
         .pipe(cleancss())
-        .pipe(gulp.dest(''));
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('./css/'));
+
 });
 
+
 gulp.task('watch', function () {
-    gulp.watch('style/scss/**/*.scss', gulp.parallel('styles'));
+    gulp.watch('./scss/**/*.scss', gulp.parallel('styles'));
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
 gulp.task('default',  gulp.parallel('styles', 'watch'));
